80ebf92dfe47f347b4bb0504299c2973f5056741
SuperNova <52492864+loverman1@users.noreply.github.com>
2020-04-20 06:14:25 +0200

    Add files via upload

--- src/Demo_prof_data.py
+++ src/Demo_prof_data.py
@@ -8,12 +8,14 @@ from Neural_Network import Neural, Neural_pro
 import numpy as np
 import scipy.special
 from matplotlib import pyplot as plt
+import math
+import warnings
 
+warnings.filterwarnings("ignore")
 np.random.seed(1)
 
 #processing data 
 #convert function to turn a string to list of int element
-
 def convert(string):
     liste = []
     for elm in string:
@@ -28,6 +30,7 @@ training_data_file.close()
 #preprocessing data 
 x_train = []
 y_train = []
+
 for elm in training_data_list:
     elm = elm.replace('\n','').replace(',','').replace(' ','')
     x = convert(elm)
@@ -35,6 +38,7 @@ for elm in training_data_list:
     target = np.zeros(10)
     target[x[-1]] = 1
     y_train.append(target)
+    pass
 
 #upload data for test 
 testing_data_file = open('test.txt', 'r')
@@ -46,92 +50,214 @@ test = []
 for elm in testing_data_list:
     elm = elm.replace('\n','').replace(',','').replace(' ','')
     test.append((convert(elm)))
+    pass
+
+def printInformation(fonction_activation, liste_alphas, liste_scores):
+    s = " Activation function "+str(fonction_activation)+"\n"
+    s += "___________________________________________________"+"\n"
+    i = 0
+    for elm in liste_alphas:
+        s += "training test with alpha : {0:4f}, score : {1:2d}".\
+                format(elm, liste_scores[i])+"/20"+"\n"
+        i +=1
+        pass
+    return s
 
 
 #test with activation function 
+""" ====================== activation functions ====================== """
 # Sigmoid functin and his derive
-activation_function  = lambda x:scipy.special.expit(x)
-derive = lambda x : x*(1.0 - x)    
- 
-#optimization
-#learning rate 
-alphas = [1, 0.01, 0.03, 0.05, 0.06, 0.07, 0.08, 0.09, 0.14]
-#alphas = [0.001]
-dropouts = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+sigmoid  = lambda x:scipy.special.expit(x)
+sigmoid_derive = lambda x : x*(1.0 - x)    
 
+# Integral_Sigmoid functin and his derive 
+Integral_Sigmoid = lambda x : np.log( 1.0 + np.exp(x) )
+sigmoid  = lambda x:scipy.special.expit(x)
+
+#activation function tan
+thn = lambda x : np.tanh(x) 
+thn_der = lambda x : 1+x**2
+
+#fonction d'activation arctang
+arctang     = lambda x : np.arctan(x)
+arctang_der = lambda x: 1/ (math.pi * (1+x*x) +0.5)
+
+
+alphas = [1, 0.01, 0.03, 0.05, 0.06, 0.07, 0.08, 0.09, 0.14]
+#alphas = [0.00003, 0.005, 0.002, 0.001,0.1,0.2,0.3,0.5]
 """
-print(' neural network with one hidden layer ')
+score1 = []
+score2 = []
+score3 = []
+score4 = []
+
+score11 = []
+score22 = []
+score33 = []
+score44 = []
+
+print(' neural network with one hidden layer')
 
 for alpha in alphas:
-    print('_'*50)
-    print('trainning with alpha:', alpha)
-    n = Neural(48, 20, 10, alpha, activation_function, derive)
-    n.fit(x_train, y_train, 2000)
-    #n.plot_Neural_error(alpha)
+    n1 = Neural(48, 32, 10, alpha, arctang, arctang_der)
+    n1.fit(x_train, y_train, 2500)
+    n1.plot_Neural_error(alpha, " arctang ")
     cpt = 0
     for elm in test:
         x = elm[-1]
-        y = np.argmax(n.query(elm[:48]))
+        y = np.argmax(n1.query(elm[:48]))
         if( x == y):
             cpt+=1
-    print(cpt,"/20")
-
-
-print(' neural network with DeopOut ')
-
-for alpha in alphas:
-    print('_'*50)
-    print('trainning with alpha:', alpha)
-    n1 = Neural(48, 60, 10, alpha, activation_function, derive)
-    for dropout in dropouts:
-        print("dropout :",dropout)
-        n1.fit(x_train, y_train, 2000, True, 0.7 )
-        n1.plot_Neural_error(alpha)
-        cpt = 0
-        for elm in test:
-            x = elm[-1]
-            y = np.argmax(n1.query(elm[:48]))
-            if( x == y):
-                cpt+=1
-        print(cpt,"/20")
-
-"""
-   
-print(' neural network with two hidden layer ')
+    score1.append(cpt)
+    pass
+print(printInformation("arctang", alphas, score1))
 
 for alpha in alphas:
-    print('_'*50)
-    print('trainning with alpha:', alpha)
-    n2 = Neural_pro(48, 80, 15, 10, alpha, activation_function, derive)
-    n2.fit(x_train, y_train, 2000)
-    #n2.plot_Neural_error(alpha)
+    n2 = Neural(48, 32, 10, alpha, Integral_Sigmoid, sigmoid)
+    n2.fit(x_train, y_train, 2500)
+    n2.plot_Neural_error(alpha, "Intergral_sigmoid")
     cpt = 0
     for elm in test:
         x = elm[-1]
         y = np.argmax(n2.query(elm[:48]))
         if( x == y):
             cpt+=1
-    print(cpt,"/20")
+    score2.append(cpt)
+    pass
+print(printInformation("integral_sigmoid", alphas, score2))
 
-
-
-
-
-def plot_result(liste, alpha, dropout):
-    EvolutionAlpha = plt.figure("The mean error")
-    
-    ax = EvolutionAlpha.add_subplot(1, 1, 1)
-    ax.plot(liste, label = "alpha:"+str(alpha)+" dropout "+str(dropout))
-        
-    ax.set_xlabel("  ")
-    ax.set_ylabel("  ")
-    ax.set_title('the mean error with different value of alpha ')
-        
-    ax.grid(True, linestyle='-.')
-    ax.legend()
-    plt.show()
+for alpha in alphas:
+    n3 = Neural(48, 32, 10, alpha, thn, thn_der)
+    n3.fit(x_train, y_train, 2500)
+    n3.plot_Neural_error(alpha, "tangante")
+    cpt = 0
+    for elm in test:
+        x = elm[-1]
+        y = np.argmax(n3.query(elm[:48]))
+        if( x == y):
+            cpt+=1
+    score3.append(cpt)
     pass
 
+print(printInformation("thang", alphas, score3))
+
+
+for alpha in alphas:
+    n4 = Neural(48, 32, 10, alpha, sigmoid, sigmoid_derive)
+    n4.fit(x_train, y_train, 2500)
+    n4.plot_Neural_error(alpha, " Sigmoid ")
+    cpt = 0
+    for elm in test:
+        x = elm[-1]
+        y = np.argmax(n4.query(elm[:48]))
+        if( x == y):
+            cpt+=1
+    score4.append(cpt)
+    pass
+print(printInformation("Sigmoid", alphas, score4))
+
+
+print('neural network with two hidden layers')
+
+for alpha in alphas:
+    n11 = Neural_pro(48, 32, 16, 10, alpha, arctang, arctang_der)
+    n11.fit(x_train, y_train, 2500)
+    n11.plot_Neural_error(alpha, " arctang ")
+    cpt = 0
+    for elm in test:
+        x = elm[-1]
+        y = np.argmax(n11.query(elm[:48]))
+        if( x == y):
+            cpt+=1
+    score11.append(cpt)
+    pass
+print(printInformation("arctang", alphas, score11))
+
+
+for alpha in alphas:
+    n22 = Neural_pro(48, 32, 16, 10, alpha, Integral_Sigmoid, Integral_Sigmoid)
+    n22.fit(x_train, y_train, 2500)
+    n22.plot_Neural_error(alpha, " Integral_Sigmoid ")
+    cpt = 0
+    for elm in test:
+        x = elm[-1]
+        y = np.argmax(n22.query(elm[:48]))
+        if( x == y):
+            cpt+=1
+    score22.append(cpt)
+    pass
+print(printInformation("Integral_Sigmoid", alphas, score22))
+
+for alpha in alphas:
+    n33 = Neural_pro(48, 32, 16, 10, alpha, thn, thn_der)
+    n33.fit(x_train, y_train, 2500)
+    n33.plot_Neural_error(alpha, " tangante ")
+    cpt = 0
+    for elm in test:
+        x = elm[-1]
+        y = np.argmax(n33.query(elm[:48]))
+        if( x == y):
+            cpt+=1
+    score33.append(cpt)
+    pass
+print(printInformation("tangante", alphas, score33))
+
+for alpha in alphas:
+    n44 = Neural_pro(48, 32, 16, 10, alpha, sigmoid, sigmoid_derive)
+    n44.fit(x_train, y_train, 2500)
+    n44.plot_Neural_error(alpha, " Sigmoid ")
+    cpt = 0
+    for elm in test:
+        x = elm[-1]
+        y = np.argmax(n44.query(elm[:48]))
+        if( x == y):
+            cpt+=1
+    score44.append(cpt)
+    pass
+print(printInformation("Sigmoid", alphas, score44))
+
+
+
+
+
+"""
+
+
+
+dropouts = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+
+scored1 = []
+scored2 = []
+
+for alpha in alphas:
+    n33 = Neural_pro(48, 32, 16, 10, alpha, thn, thn_der)
+    n33.fit(x_train, y_train, 2500)
+    n33.plot_Neural_error(alpha, " tangante ")
+    cpt = 0
+    for elm in test:
+        x = elm[-1]
+        y = np.argmax(n33.query(elm[:48]))
+        if( x == y):
+            cpt+=1
+    scored1.append(cpt)
+    pass
+print(printInformation("tangante", alphas, scored1))
+
+
+for alpha in alphas:
+    n33 = Neural_pro(48, 32, 16, 10, alpha, thn, thn_der)
+    for drop in dropouts:
+        print('drop out with ',drop)
+        n33.fit(x_train, y_train, 2500, True, drop)
+        n33.plot_Neural_error(alpha, " tangante ")
+        cpt = 0
+        for elm in test:
+            x = elm[-1]
+            y = np.argmax(n33.query(elm[:48]))
+            if( x == y):
+                cpt+=1
+        scored2.append(cpt)
+    print(printInformation("tangante", alphas, scored2))
 
 
 
@@ -143,11 +269,3 @@ def plot_result(liste, alpha, dropout):
 
 
 
-
-
-
-
-
-
-
-   
--- src/Neural_Network.py
+++ src/Neural_Network.py
@@ -8,15 +8,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 class Neural():
-    def __init__(self, input_nodes, hidden_nodes, output_nodes, learningRate, activation_function, derive_function):
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learningRate, 
+                 activation_function, derive_function):
         #initialize the nueral nodes 
         self.input_nodes  = input_nodes
         self.hidden_nodes = hidden_nodes
         self.output_nodes = output_nodes
         
         #initializing theweights randomly
-        self.weights1 = np.random.normal(0.0, pow(self.hidden_nodes, -0.5), (self.input_nodes, self.hidden_nodes))
-        self.weights2 = np.random.normal(0.0, pow(self.output_nodes, -0.5), (self.hidden_nodes, self.output_nodes))
+        self.weights1 = np.random.normal(0.0, pow(self.hidden_nodes, -0.5), 
+                                        (self.input_nodes, self.hidden_nodes))
+        self.weights2 = np.random.normal(0.0, pow(self.output_nodes, -0.5),
+                                        (self.hidden_nodes, self.output_nodes))
 
         #activation function
         self.activation_function  = activation_function
@@ -32,10 +35,15 @@ class Neural():
         
         for e in range(epochs):
             input_layer  = x_train
-            hidden_layer = self.activation_function(np.dot(input_layer, self.weights1 ))
+            hidden_layer = self.activation_function(np.dot(input_layer, 
+                                                           self.weights1 ))
             if(dropOut):
-                hidden_layer *= np.random.binomial([np.ones((len(input_layer),self.hidden_nodes))],1-dropOut_rate)[0] * (1.0/(dropOut_rate))
-            output_layer = self.activation_function(np.dot(hidden_layer, self.weights2))
+                hidden_layer *= np.random.binomial(
+                    [np.ones((len(input_layer), self.hidden_nodes))],
+                    1-dropOut_rate)[0] * (1.0/(dropOut_rate))
+                
+            output_layer = self.activation_function(np.dot(hidden_layer,
+                                                           self.weights2))
             
             error         = y_train - output_layer
             modification2 = error * self.derive(output_layer)
@@ -44,18 +52,22 @@ class Neural():
             
             self.liste_error.append(np.mean(error))
         
-            self.weights1 +=  self.learningRate * np.dot(input_layer.T,  modification1)
-            self.weights2 +=  self.learningRate * np.dot(hidden_layer.T, modification2)
+            self.weights1 +=  self.learningRate * np.dot(input_layer.T, 
+                                                         modification1)
+            self.weights2 +=  self.learningRate * np.dot(hidden_layer.T, 
+                                                         modification2)
         pass
     
     def query(self, x_test):
         input_layer  = x_test
-        hidden_layer = self.activation_function(np.dot(input_layer, self.weights1))
-        output_layer = self.activation_function(np.dot(hidden_layer, self.weights2))
+        hidden_layer = self.activation_function(np.dot(input_layer,
+                                                       self.weights1))
+        output_layer = self.activation_function(np.dot(hidden_layer, 
+                                                       self.weights2))
         return output_layer
     
-    def plot_Neural_error(self, alpha):
-        EvolutionAlpha = plt.figure("The mean error")
+    def plot_Neural_error(self, alpha, activation_function):
+        EvolutionAlpha = plt.figure("The mean error "+str(activation_function))
         
         ax = EvolutionAlpha.add_subplot(1, 1, 1)
         ax.plot(self.liste_error, label = "alpha:"+str(alpha) )
@@ -63,16 +75,19 @@ class Neural():
         ax.set_xlabel(" generation")
         ax.set_ylabel(" error ")
         ax.set_title(' the mean error with different value of alpha ')
+
+        #ax.set_xlim(0)
+        #ax.set_ylim(0)
         
         ax.grid(True, linestyle='-.')
         ax.legend()
-        plt.show()
-        
+        plt.show()        
         pass
         
 
 class Neural_pro():
-    def __init__(self, input_nodes, hidden_nodes1, hidden_nodes2, output_nodes, learningRate, activation_function, derive_function):
+    def __init__(self, input_nodes, hidden_nodes1, hidden_nodes2, output_nodes, 
+                 learningRate, activation_function, derive_function):
         #initialize the nueral nodes 
         self.input_nodes  = input_nodes
         self.hidden_nodes1 = hidden_nodes1
@@ -80,9 +95,12 @@ class Neural_pro():
         self.output_nodes = output_nodes
         
         #initializing theweights randomly
-        self.weights1 = np.random.normal(0.0, pow(self.hidden_nodes1, -0.5), (self.input_nodes, self.hidden_nodes1))
-        self.weights2 = np.random.normal(0.0, pow(self.hidden_nodes2, -0.5), (self.hidden_nodes1, self.hidden_nodes2))
-        self.weights3 = np.random.normal(0.0, pow(self.output_nodes, -0.5), (self.hidden_nodes2, self.output_nodes))
+        self.weights1 = np.random.normal(0.0, pow(self.hidden_nodes1, -0.5), 
+                                    (self.input_nodes, self.hidden_nodes1))
+        self.weights2 = np.random.normal(0.0, pow(self.hidden_nodes2, -0.5), 
+                                    (self.hidden_nodes1, self.hidden_nodes2))
+        self.weights3 = np.random.normal(0.0, pow(self.output_nodes, -0.5), 
+                                    (self.hidden_nodes2, self.output_nodes))
 
         #activation function
         self.activation_function  = activation_function
@@ -93,16 +111,26 @@ class Neural_pro():
         self.liste_error = []
         pass
     
-    def fit(self, x_train, y_train, epochs):
+    def fit(self, x_train, y_train, epochs, dropOut = False, dropOut_rate = 0):
         x_train = np.array(x_train, ndmin = 2)
         y_train = np.array(y_train, ndmin = 2)
         
         for e in range(epochs):
             
             input_layer   = x_train
-            hidden_layer1 = self.activation_function(np.dot(input_layer, self.weights1))
+            hidden_layer1 = self.activation_function(np.dot(input_layer, 
+                                                            self.weights1))
+            if(dropOut):
+                hidden_layer1 *= np.random.binomial(
+                    [np.ones((len(input_layer),self.hidden_nodes1))],
+                    1-dropOut_rate)[0] * (1.0/(dropOut_rate))
             hidden_layer2 = self.activation_function(np.dot(hidden_layer1, self.weights2))
-            output_layer  = self.activation_function(np.dot(hidden_layer2, self.weights3))
+            if(dropOut):
+                hidden_layer2 *= np.random.binomial(
+                    [np.ones((len(hidden_layer1),self.hidden_nodes2))],
+                    1-dropOut_rate)[0] * (1.0/(dropOut_rate))
+            output_layer  = self.activation_function(np.dot(hidden_layer2, 
+                                                            self.weights3))
             
             error         = y_train - output_layer
             modification  = error * self.derive(output_layer)
@@ -115,20 +143,26 @@ class Neural_pro():
             
             self.liste_error.append(np.mean(error))
                   
-            self.weights1 += self.learningRate * np.dot(input_layer.T, modification1)
-            self.weights2 += self.learningRate * np.dot(hidden_layer1.T, modification2)
-            self.weights3 += self.learningRate * np.dot(hidden_layer2.T, modification)
+            self.weights1 += self.learningRate * np.dot(input_layer.T, 
+                                                        modification1)
+            self.weights2 += self.learningRate * np.dot(hidden_layer1.T, 
+                                                        modification2)
+            self.weights3 += self.learningRate * np.dot(hidden_layer2.T, 
+                                                        modification)
             pass
     
     def query(self, test):
         input_layer   = test
-        hidden_layer1 = self.activation_function(np.dot(input_layer, self.weights1))
-        hidden_layer2 = self.activation_function(np.dot(hidden_layer1, self.weights2))
-        output_layer  = self.activation_function(np.dot(hidden_layer2, self.weights3))
+        hidden_layer1 = self.activation_function(np.dot(input_layer, 
+                                                        self.weights1))
+        hidden_layer2 = self.activation_function(np.dot(hidden_layer1, 
+                                                        self.weights2))
+        output_layer  = self.activation_function(np.dot(hidden_layer2, 
+                                                        self.weights3))
         return output_layer
     
-    def plot_Neural_error(self, alpha):
-        EvolutionAlpha = plt.figure("The mean error")
+    def plot_Neural_error(self, alpha, activation_function):
+        EvolutionAlpha = plt.figure("The mean error "+str(activation_function))
         
         ax = EvolutionAlpha.add_subplot(1, 1, 1)
         ax.plot(self.liste_error, label = "alpha:"+str(alpha) )
@@ -137,16 +171,10 @@ class Neural_pro():
         ax.set_ylabel(" error ")
         ax.set_title('the mean error with different value of alpha ')
         
+        #ax.set_xlim(0)
+        #ax.set_ylim(0)
+
         ax.grid(True, linestyle='-.')
         ax.legend()
-        plt.show()
-        
+        plt.show()    
         pass
-
-            
-
-    
-    
-    
- 
-    
